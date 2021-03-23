d647cf04479bb4b95859643411acd3d01561613b
Kevin Nguyen <kenguyen@et.esiea.fr>
2021-03-23 18:12:05 +0100

    Development Accueil page

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Main/>
   </div>
 </template>
 
+<script>
+import Main from "./components/main/main"
+
+export default {
+  name: 'App',
+  components: {
+    Main
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body{
+  overflow: hidden;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.router-view{
+  position: absolute;
+  overflow-x: hidden;
+  overflow-y: auto;
+  left: 200px;
+  top: 0;
+  height: 100%;
+  width: 1720px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Accueil from '../components/accueil/accueil.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Accueil',
+    component: Accueil
   },
   {
     path: '/about',
